ea2e2c47c467f19dc2638a58d1184d332c6b8fc8
C4RM3L0 <saad@admin.com>
2023-09-25 20:42:57 -0400

    5

---
+++ c02/ft_str_is_uppercase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int ft_str_is_lowercase(char *str)
+{
+    int    i;
+    i = 0;
+    
+    while(str[i])
+    {   
+        
+        if (!(str[i] > 64 && str[i] < 91))
+        {
+            printf("Lower Case");
+            return 0;
+        }
+        i++;
+    }
+    printf("Upper Case");
+    return 1;
+}
+
+int main()
+{
+    char src[] = "HELLO";
+    ft_str_is_lowercase(src);
+}
